3aafc1c2293464ed975937d0733baebbf5fe6dc1
Tony <antonivelasquez67@gmail.com>
2024-03-26 19:09:06 -0400

    TOFIX: Styles.css bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Production",
+      template: "./src/index.html",
     }),
   ],
-  output: {
-    filename: "[name].main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
       },
     ],
   },
