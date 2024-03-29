83a957d14ca37d3cf0ee019bf4c99702aeb9a2f1
Tony <antonivelasquez67@gmail.com>
2024-03-29 15:36:11 -0400

    TODO: Select the DOM ELEMENTS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const btnHome = document.querySelector(".btn-home");
+const btnMenu = document.querySelector(".btn-menu");
+const btnAbout = document.querySelector(".btn-about");
+
 import "./styles.css";
 import img from "../animal.png";
 import { container } from "./menu.js";
